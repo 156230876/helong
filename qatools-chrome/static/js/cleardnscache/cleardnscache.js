f4f407f503065dc16292c673570ed57f9949e600
helong <helong@kuaishou.com>
2021-06-09 18:00:20 +0800

    删除插件

--- qatools-chrome/static/js/cleardnscache/cleardnscache.js
+++
@@ -1,5 +0,0 @@
-_clearDnsCache = function () {
-    alert("do _clearDnsCache");
-    //document.getElementById("capture-status-view-actions").value = "flush-sockets";
-};
-_clearDnsCache();
--- qatools-chrome/static/js/qatools-content.js
+++
@@ -1,4 +0,0 @@
-//国内政策修改页处理
-function doInjectFuwu(){
-	alert("嘿嘿");
-}
